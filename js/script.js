2d1612141c214651b2fa64afe7d70ca284fef49b
Saran B M <saran.b.m.001@gmail.com>
2022-07-11 22:03:56 +0530

    Add Book Form

--- js/script.js
+++ js/script.js
@@ -47,95 +47,112 @@ const Library = (function(){
 
 const DisplayController = (function() {
     
-    let body = document.querySelector('body');
     let cards = document.querySelector('.cards');
     let addBookButton = document.querySelector('button');
+    let titleField = document.querySelector('#title');
+    let authorField = document.querySelector('#author');
+    let pagesField = document.querySelector('#pages');
+    let add = document.querySelector('#add');
 
-    function addEventListeners(){
+    (function addEventListeners(){
         addBookButton.addEventListener('click', addBook);
+    })();
+
+    function addBook(events, defaultBook=''){
+            
+            if(!defaultBook){
+                let newBook;
+                newBook = new Book;
+                newBook.title = titleField.value;
+                newBook.author = authorField.value;
+                newBook.pages = pagesField.value;
+                Library.addBook(newBook);
+
+                let card = createCard(newBook);
+                cards.appendChild(card);
+            }
+           
+            Library.addBook(defaultBook);
+
+            let card = createCard(defaultBook);
+            cards.appendChild(card);
+        
+        
+        
     }
 
-    function addBook(){
-        let newBook = new Book;
-        newBook.title = prompt("Title");
-        newBook.author = prompt("author");
-        newBook.pages = prompt("pages");
 
-        Library.addBook(newBook);
-
-        showBooks();
-    }
+    function createCard(book){
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.classList.add(book.title);
+        let title = document.createElement('h1');
+        let author = document.createElement('h3');
+        let pages = document.createElement('h3');
+        let read = document.createElement('p');
+        
+    
+        let removeBookButton = document.createElement('button');
+        removeBookButton.textContent = "Remove";
+        removeBookButton.classList.add('removeBook');
+        removeBookButton.setAttribute("data-title", book.title);
+        removeBookButton.addEventListener('click', removeBook)
+    
+        let markReadButton = document.createElement('button');
+        markReadButton.classList.add('markRead');
+        markReadButton.setAttribute("data-title", book.title);
+        markReadButton.textContent = "Read";
+        markReadButton.addEventListener('click', markBookRead);
+        
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        read.textContent = book.read?'Read':'Not Read Yet';
+        
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+        card.appendChild(removeBookButton);
+        card.appendChild(markReadButton);
+        
+        return card;
+        }
 
     function showBooks(){
-        if(cards) body.removeChild(cards);
-        cards = document.createElement('div');
-        cards.classList.add('cards');
-        for (let book of Library.getShelf()){
-            let card = document.createElement('div');
-            card.classList.add('card');
-            let title = document.createElement('h1');
-            let author = document.createElement('h3');
-            let pages = document.createElement('h3');
-            let read = document.createElement('p');
-    
-            let removeBookButton = document.createElement('button');
-            removeBookButton.textContent = "Remove";
-            removeBookButton.classList.add('removeBook');
-            removeBookButton.setAttribute("data-title", book.title);
-            removeBookButton.addEventListener('click', removeBook)
-    
-            let markReadButton = document.createElement('button');
-            markReadButton.classList.add('markRead');
-            markReadButton.setAttribute("data-title", book.title);
-            markReadButton.textContent = "Read";
-            markReadButton.addEventListener('click', markBookRead);
-            
-            title.textContent = book.title;
-            author.textContent = book.author;
-            pages.textContent = book.pages;
-            read.textContent = book.read?'Read':'Not Read Yet';
-            
-            card.appendChild(title);
-            card.appendChild(author);
-            card.appendChild(pages);
-            card.appendChild(read);
-            card.appendChild(removeBookButton);
-            card.appendChild(markReadButton);
-            cards.appendChild(card);
-        }
-        
-        body.appendChild(cards);
-        addEventListeners();
+
     }
 
+
     function markBookRead(e){
-        let title = e.target.getAttribute("data-title");
+        let title = e.target.getAttribute("data-title")
+        let markedBook = cards.querySelector(`.${title}>p`);
         Library.markBookRead(title);
+        markedBook.textContent = 'Read'
         showBooks();
     
     }
 
     function removeBook(e){
-    
-        let title = e.target.getAttribute("data-title");
+        let title = e.target.getAttribute("data-title")
+        let removedBook = cards.querySelector(`.${title}`);
         Library.removeBook(title);
+        cards.removeChild(removedBook);
         showBooks();
         
     
 }
-    return {showBooks}
+    return {addBook}
 })();
 
 let b1 = new Book('ycw', 'askdfj', 123);
 let b2 = new Book('ikigai', 'askdfj', 123);
 let b3 = new Book('masth', 'askdfj', 123);
 
-Library.addBook(b1);
-Library.addBook(b2);
-Library.addBook(b3);
+DisplayController.addBook('', b1);
+DisplayController.addBook('', b2);
+DisplayController.addBook('', b3);
 
-
-DisplayController.showBooks();
 
 
 
